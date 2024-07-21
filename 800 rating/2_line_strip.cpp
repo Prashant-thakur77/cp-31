888ff9f35365b6796b26bd7d03fa42ab8c31b701
PRASHANT THAKUR <prashant101007@gmail.com>
2024-07-21 13:27:08 +0530

    2nd line strip

---
+++ 800 rating/2_line_strip.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    
+    cin>>t;
+    while(t--){
+        int n,x;
+        int m,c,u,o;
+        cin>>n;
+        cin>>x;
+        vector<int> v(n);
+        for(int i=0;i<n;i++){
+            cin>>v[i];
+            
+        }    
+        c=v[0];
+        for(int i=0;i<n-1;i++){
+            m=v[i+1]-v[i];
+            c=max(c,m);
+        }    
+        o=2*(x-(v[n-1])); 
+        
+        cout<<max(c,o)<<endl; 
+
+    }
+    return 0;
+}
