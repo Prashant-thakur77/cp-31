5c4bbd007d1d0c904d76ce8e0b7be2a0e9672cb6
PRASHANT THAKUR <prashant101007@gmail.com>
2024-08-01 22:29:44 +0530

    1st 900

--- 800 rating/1st_900_rating_forked.cpp
+++ 800 rating/1st_900_rating_forked.cpp
@@ -4,26 +4,27 @@ using namespace std;
     int t;
     cin>>t;
     while(t--){
-        long long a,b;
+        int a,b;
         cin>>a>>b;
-        long long k1,k2;
+        int k1,k2;
         cin>>k1>>k2;
-        long long q1,q2;
+        int q1,q2;
         cin>>q1>>q2;
         vector<pair<int,int>> d={{a,b},{a,-b},{-a,b},{-a,-b},
                                 {b,a},{b,-a},{-b,a},{-b,-a}};
         set <pair<int,int>> st1,st2;
-        for(auto t:d){
-            int x= k1+t.first;
-            int y=k2+t.second;
+        for(auto p:d){
+            int x= k1+p.first;
+            int y=k2+p.second;
             st1.insert(make_pair(x,y));
-            x=q1 + t.first;
-            y=q2+t.second;
+            x=q1 + p.first;
+            y=q2+p.second;
             st2.insert(make_pair(x,y));
         }                        
         int ans=0;
         for(auto pos:st1){
-            if(st2.find(pos)!=st1.end()) ans++;
+            if(st2.find(pos)!=st2.end()) 
+                ans++;
 
         }
         cout<<ans<<endl;
