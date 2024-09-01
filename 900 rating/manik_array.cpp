c91300105d1b4d66d419c2f77eb511e2cf0251d0
PRASHANT THAKUR <prashant101007@gmail.com>
2024-09-02 00:44:00 +0530

    cp 31 900

---
+++ 900 rating/manik_array.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int main(){
+    int t;
+    cin>>t;
+    while(t--){
+      int n;
+      cin>>n;
+      vector<int> v(n+1);
+      for(int i=1;i<=n;i++){
+        cin>>v[i];
+      }
+      int min=v[0];
+      int maxi=v[n]-v[1];
+      for(int i=1;i<n;i++){
+        maxi=max(maxi,v[n]-v[i]);
+      }
+      for(int i=2;i<=n;i++){
+        maxi=max(maxi,v[i]-v[1]);
+      }
+      for(int i=1;i<n;i++){
+        maxi=max(maxi,v[i]-v[i+1]);
+      }
+      cout<<maxi<<endl;
+        
+
+      
+
+    
+    return 0;
+ }
+ 
+ 
