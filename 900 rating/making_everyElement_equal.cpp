af0ab71be9b7556421b48068ad6ac62490c2936e
PRASHANT THAKUR <prashant101007@gmail.com>
2024-09-13 20:07:39 +0530

    solved cp900 array clonning technique

--- 900 rating/making_everyElement_equal.cpp
+++ 900 rating/making_everyElement_equal.cpp
@@ -10,5 +10,31 @@ using namespace std;
       for(int i=0;i<n;i++){
         cin>>v[i];
       }
-      int max_no=0;
+      int max_no=1;
       int moves=0;
+      sort(v.begin(),v.end());
+     
+      int cnt=1;
+      for(int i=1;i<n;i++){
+        if(v[i]==v[i-1]){
+          cnt++;
+          
+        }
+        else{
+          cnt=1;
+        }
+        if(cnt>max_no){
+          max_no=cnt;
+        }
+      }
+      while(max_no<=n/2){
+        max_no=max_no*2;
+        moves+=1+(max_no-max_no/2);
+      }
+      if(max_no<n){
+        moves+=1+(n-max_no);
+      }
+      cout<<moves<<endl;
+    }
+    return 0;
+ }
