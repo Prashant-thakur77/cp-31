fe5628777c881908949dc735aa612e218c5fd2cb
PRASHANT THAKUR <prashant101007@gmail.com>
2024-09-15 22:58:47 +0530

    just submit

--- 900 rating/make_ap.cpp
+++ 900 rating/make_ap.cpp
@@ -7,3 +7,49 @@ int main(){
     
 
     while (t--){
+      int flag=0;
+      long long a,b,c;
+      cin>>a>>b>>c;
+     
+     
+      if(a==2*b-c){
+        flag=1;
+
+      }
+      else{
+        long long a1,c1;
+        long long b1;
+        
+        
+        a1=2*b-c;
+        b1=(a+c)/(float)2;
+        c1=2*b-a;
+        double b2=(a+c)/(float)2;
+        cout<<"hello"<<endl;
+        cout<<a1<<"  "<<b1<<"   "<<c1<<endl;
+        if(a1>0 && a1%a==0){
+          flag=1;
+          cout<<"helloa"<<endl;
+        }
+        if(b2==b1){
+           if(b1>0 && b1%b==0 ){
+          flag=1;
+          cout<<"hellob"<<endl;
+        }
+
+        }
+       
+        if(c1>0 && c1%c==0){
+          flag=1;
+          cout<<"helloc"<<endl;
+        }
+      }
+      if(flag==0){
+        cout<<"NO"<<endl;
+      }
+      else{
+        cout<<"YES"<<endl;
+      }
+    }
+    return 0;
+}
