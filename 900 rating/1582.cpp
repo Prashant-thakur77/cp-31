4f7ab7877572f7fd0e312be7780342bddffae489
PRASHANT THAKUR <prashant101007@gmail.com>
2024-09-18 23:10:55 +0530

    1582

---
+++ 900 rating/1582.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int main(){
+    int t;
+    cin>>t;
+    while(t--){
+      int n;
+      cin>>n;
+      vector<long long> v(n);
+      int flag=0;
+      long long cnt=0;
+      long long cnt0=0;
+      for(int i=0;i<n;i++){
+        cin>>v[i];
+        if(v[i]==1) {
+          flag=1;
+          cnt++;
+        }
+        else if(v[i]==0){
+          cnt0++;
+        }
+      }
+      if(cnt==0){
+        cout<<0<<endl;
+      }
+      else{
+        cout<<cnt*(1<<cnt0)<<endl;
+      }
+      
+    }
+    return 0;
+ }
