0379a865168eb91a768b0bad4f6c7201e4382ced
PRASHANT THAKUR <prashant101007@gmail.com>
2024-10-28 20:53:12 +0530

    done

---
+++ codeforces/2026a.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int main(){
+    int t;
+    cin>>t;
+    while(t--){
+      long long x,y,k;
+      
+      cin>>x>>y>>k;
+      long long small=min(x,y);
+      cout<<0<<" "<<0<<" "<<small<<" "<<small<<endl;
+      cout<<0<<" "<<small<<" "<<small<<" "<<0<<endl;
+
+      
+    }
+    
+    return 0;
+ }
