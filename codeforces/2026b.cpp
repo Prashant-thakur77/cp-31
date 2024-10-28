c1abfdbdf417b5ca8781dcbf5856689bac2ec44f
PRASHANT THAKUR <prashant101007@gmail.com>
2024-10-28 22:24:16 +0530

    done

--- codeforces/2026b.cpp
+++ codeforces/2026b.cpp
@@ -11,9 +11,20 @@ using namespace std;
         cin>>v[i];
       }
       vector<long long> b;
+      long long max=0;
       for(int i=0;i<x-1;i++){
         long long t=v[i+1]-v[i];
         b.push_back(t);
+        if(b[i]>max){
+          max=b[i];
+        }
+
+
+      }
+      int flag=1;
+      if(max==b[x-2] || max==b[0]){
+        flag=0;
+
       }
       sort(b.begin(),b.end());
       if(x==1){
@@ -23,7 +34,11 @@ using namespace std;
         cout<<b[0]<<endl;
       }
       else{
-        cout<<b[x-3]<<endl;
+        if(flag==0){
+          cout<<b[x-3]<<endl;
+
+        }
+        
 
       }
       
