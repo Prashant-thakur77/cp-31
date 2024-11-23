9597c8ae22b9513bde6567a92f4eca41d99849cb
Prashant-thakur77 <prashant101007@gmail.com>
2024-11-23 21:04:22 +0530

    2039

---
+++ codeforces/2039.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int main(){
+   long long t;
+    cin>>t;
+    
+    while(t--){
+      int n;
+      cin>>n;
+      for(int i=0;i<n;i++){
+        cout<<(i+1)*1+i<<" ";
+      }
+      cout<<endl;
+
+      
+    }
+    
+    return 0;
+ }
