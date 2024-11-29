092381d07971d093235b10f500ab6d0342918a39
Prashant-thakur77 <prashant101007@gmail.com>
2024-11-29 23:54:11 +0530

    1859

---
+++ 1000 rating/1859.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+  int t;
+  cin>>t;
+  while(t--){
+    long long n;
+    cin>>n;
+    int mini=INT_MAX;
+    vector<int> min_2;
+    for(int i=0;i<n;i++){
+      long long m;
+      cin>>m;
+      vector<int> arr(m);
+      for(int i=0;i<m;i++) cin>>arr[i];
+      int min_el=INT_MAX;
+      int ind=0;
+      for(int i=0; i<m; i++){
+        if(arr[i]<min_el){
+          min_el = arr[i];
+          ind=i;
+
+        }
+        
+
+      }
+      mini=min(min_el,mini);
+      arr.erase(arr.begin()+ind);
+      int minnn=INT_MAX;
+      for(int i=0; i<m; i++){
+        if(arr[i]<minnn){
+          minnn = arr[i];
+          ind=i;
+
+        }
+        
+
+      }
+      min_2.push_back(minnn);
+
+
+    }
+    int mi_2=INT_MAX;
+    for(int i=0; i<min_2.size(); i++){
+        if(min_2[i]<mi_2){
+          mi_2 = min_2[i];
+          
+
+        }
+        
+
+      }
+    long long beauty=mini+accumulate(min_2.begin(),min_2.end(),0)-mi_2;
+    cout<<beauty<<endl;
+    
+    
+
+
+  }
+  return 0;
+}
