c2243fbcfb5eb980a3fe97316adf97649e649364
Prashant-thakur77 <prashant101007@gmail.com>
2025-01-23 23:24:20 +0530

    done

--- codeforces/codewrap3.cpp
+++ codeforces/codewrap3.cpp
@@ -1,5 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+int f(int i,int j,string& a,string& b,vector<vector<int>>& ans){
+    if(i<0) return 0+j+1;
+    if(j<0) return i+1;
+    if(ans[i][j]!=-1) return ans[i][j];
+    if(a[i]==b[j]) return ans[i][j]=f(i-1,j-1,a,b,ans);
+ 
+
+}
 
 int main() {
     string a, b;
@@ -9,7 +17,8 @@ int main() {
     cin >> x >> y;
 
     int m = a.size(), n = b.size();
-    vector<vector<int>> ans(m + 1, vector<int>(n + 1));
+    vector<vector<int>> ans(m + 1, vector<int>(n + 1,-1));
+    return f(n,m,a,b,ans);
 
     for (int i = 0; i <= m; i++) ans[i][0] = i * x;
     for (int j = 0; j <= n; j++) ans[0][j] = j * x;
