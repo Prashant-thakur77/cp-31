0f39bef77ecce00fc1467b59ec9c24fcef28db84
Prashant-thakur77 <prashant101007@gmail.com>
2025-07-31 16:15:36 +0530

    done

--- codeforces/add.cpp
+++ codeforces/add.cpp
@@ -7,56 +7,20 @@ int main(){
     long long a;
     long long b;
     cin>>a>>b;
-    int i=0;
-    int flag=0;
-    int cnt=0;
-    int ans=INT_MAX;
-    if(b>a){
-      flag=1;
-      ans=1;
-    }
-    if(b==a){
-      ans=2;
-      flag=1;
-    }
-    while(flag==0){
-      int copy=a;
-      
-      if(b==1 && i==0){
-        i++;
-       
-      }
-      int div=b+i;
-      if(div>a){
-        cnt=1;
-      }
-      
-      while((copy/div)!=0 && a!=b){
-   
-        
-        copy=copy/div;
-       //cout<<"copy   "<<copy<<endl;
-        cnt++;
-        //cout<<"cnt   "<<cnt<<endl;
-        if(copy<div){
-          cnt++;
-          continue;
+    int ans=1e9;
+    for(int i=0;i<10;i++){
+      long long c=a;
+      if((b+i)>=2){
+        int turns=i;
+        while (c>0)
+        {
+          c=c/(b+i);
+          turns++;
         }
-      }
-      int real=cnt+i;
-      //cout<<"cnt2   "<<cnt<<endl;
-    
-      //cout<<" div"<<div<<endl;
-      //cout<<" real"<<real<<endl;
-      if(real>ans){
-        flag==1;
-        break;
+        ans=min(turns,ans);
+        
 
       }
-      ans=min(ans,real);
-   
-      cnt=0;
-      i++;
     }
     cout<<ans<<endl;
   }
