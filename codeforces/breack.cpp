b7c21503d9f2bea4bfce042d97ecf1565632074c
Prashant-thakur77 <prashant101007@gmail.com>
2025-08-03 10:16:51 +0530

    done

--- codeforces/breack.cpp
+++ codeforces/breack.cpp
@@ -5,56 +5,21 @@ int main(){
   
   cin>>t;
   while(t--){
-    long long n,k,b,s;
-    cin>>n>>k>>b>>s;
-    vector<long long> beauty(n,0);
-    long long lowCond=k*b;
-    long long upperCond=(n-1)*(k-1)+(k*b)+k-1;
-    int flag=0;
-    if(s<lowCond || s>upperCond){
-       cout<<-1<<endl;
-       flag=1;
-       
+    long long n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int cnt=0;
+    int mini=INT_MAX;
+
+  for(int i=0;i<n;i++){
+    if(s[i]=='('){
+      cnt++;
     }
-    if(flag==0){
-      long long cursum=0;
-    
-    for(int i=0;i<n;i++){
-          
-          if(i==0){
-
-            beauty[i]=(k*b);
-            cursum+=beauty[i];
-            
-            continue;
-          }
-          if(s-cursum<k-1){
-              beauty[i]=s-cursum;
-              cursum+=beauty[i];
-              break;
-
-
-          }
-          
-          beauty[i]=k-1;
-          
-          
-          cursum+=beauty[i];
-    }
-    while(s-cursum!=0){
-      beauty[0]++;
-      cursum++;
-    }
-    
-    for(int i=0;i<n;i++){
-          cout<<beauty[i]<<" ";
-
-      }
-    cout<<endl;
-        
-
-
+    else cnt--;
+    mini=min(mini,cnt);
   }
+  cout<<abs(mini)<<endl;
     
     
       
