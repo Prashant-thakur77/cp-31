b3360438fb4c5ac095d4faa0dbb6373305bc31ad
Prashant-thakur77 <prashant101007@gmail.com>
2025-08-04 22:29:23 +0530

    done

---
+++ 1100 rating/1.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    map<char,int> mp;
+    int cnt=0;
+    int ans=0;
+    for(int i=0;i<n;i++){
+      
+        mp[s[i]]++;
+      if(mp[s[i]]==1)cnt++;
+      ans=ans+cnt;
+
+    }
+    cout<<ans<<endl;
+  }
+  return 0;
+}
