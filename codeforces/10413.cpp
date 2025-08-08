5826ef0de99e9ddd2a0108de34fbac2b450ffbec
Prashant-thakur77 <prashant101007@gmail.com>
2025-08-08 11:50:15 +0530

    done

--- codeforces/10413.cpp
+++ codeforces/10413.cpp
@@ -1,68 +1,54 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-  int t;
-  cin>>t;
-  while(t--){
-   long long n,k;
-   cin>>n>>k;
-   vector<long long> a(n);
-   vector<long long> b(n);
-   for(int i=0;i<n;i++){
-    cin>>a[i];
-   }
-   for(int i=0;i<n;i++){
-    cin>>b[i];
-   }
-   long long min_sum=INT_MAX;
-   int ind1=-1;
-   int ind2=-1;
-   for(int i=0;i<n;i++){
-    for(int j=i+1;j<n;j++){
 
+#define ll long long
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+
+void solve() {
+    // Your code here
+    ll n,k;
+    cin>>n>>k;
+    vector<pair<ll,ll>> v(n);
     
-      priority_queue<int> pq;
-      pq.push(a[i]);
-      pq.push(a[j]);
-      pq.push(b[i]);
-      pq.push(b[j]);
-      long long maxi1=pq.top();
-      pq.pop();
-      long long maxi2=pq.top();
-      pq.pop();
-      long long mini2=pq.top();
-      pq.pop();
-      long long mini1=pq.top();
-      
-      long long sum=abs(maxi1+maxi2-(mini2+mini1));
-      if(sum<min_sum){
-        min_sum=min(min_sum,sum);
-        ind1=i;
-        ind2=j;
+    for(int i=0;i<n;i++){
+      cin>>v[i].first;
+    }
+    for(int i=0;i<n;i++){
+      cin>>v[i].second;
+    }
+    sort(all(v));
+    for(int i=0;i<n;i++){
+      if(v[i].first>v[i].second)swap(v[i].first,v[i].second);
+    }
+   
+    ll ans=0;
+    for(int i=0;i<n;i++){
+      ans+=abs(v[i].first-v[i].second);
+    }
+    for(int i=0;i<n-1;i++){
+      if(v[i+1].first<=v[i].second){
+        cout<<ans<<endl;
+    
+        return;
       }
     }
-   }
-   int flag=1;
-   long long val=0;
-   for(int i=0;i<n;i++){
-    if((i==ind1 || i==ind2)){
-      if(flag==1){
-        flag=0;
-        val+=min_sum;
-
-      }
-     
-      continue;
-
+    ll mini=LLONG_MAX;
+    for(int i=0;i<n-1;i++){
+      mini=min(mini,abs(v[i+1].first-v[i].second));
     }
-    val+=abs(a[i]-b[i]);
+    
+    cout<<ans+2*mini<<endl;
+    return;
+    
 
-   }
-   cout<<val<<endl;
+}
 
-
-
-  }
-  return 0;
-
+int32_t main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
 }
