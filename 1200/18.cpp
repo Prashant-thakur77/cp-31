a48990735dcdbcc717e17cc7cd4c92c7dcda5bf2
Prashant-thakur77 <prashant101007@gmail.com>
2025-08-26 09:18:42 +0530

    done

---
+++ 1200/18.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define mod 1000000007
+
+void solve() {
+    // Your code here
+    ll n,x;
+    cin>>n>>x;
+    vector<ll> v(n);
+    for(int i=0;i<n;i++){
+      cin>>v[i];
+    }
+    sort(all(v));
+    vector<ll> presum(n+1,0);
+    for(int i=1;i<=n;i++){
+      presum[i]=presum[i-1]+v[i-1];
+    }
+    ll ans=0;
+    for(int i=1;i<=n;i++){
+      if(presum[i]>x)break;
+      ll days=(x-presum[i])/i;
+      ans+=days+1;
+    }
+    cout<<ans<<endl;
+    return;
+
+}
+
+int32_t main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
