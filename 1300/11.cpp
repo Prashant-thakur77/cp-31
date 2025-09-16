8eb85d947999a1f73563254de6c0ed0b402af8ed
Prashant-thakur77 <prashant101007@gmail.com>
2025-09-16 11:15:24 +0530

    done

--- 1300/11.cpp
+++ 1300/11.cpp
@@ -6,15 +6,43 @@ using namespace std;
 #define all(x) (x).begin(), (x).end()
 #define pb push_back
 #define mod 1000000007
+int ans=0;
+int dfs(int node,vector<vector<int>>& v,string s){
+    if(v[node].size()==0){
+        return s[node-1]=='W'?1:-1;
+    }
+    int count=0;
+    for(auto ch: v[node]){
+        count+=dfs(ch,v,s);
+    }
+    count+=(s[node-1]=='W'? 1:-1);
+    if(count==0){
+        ans++;
+    }
+    return count;
 
-void solve() {
-    // Your code here
 }
 
 int32_t main() {
     fast_io;
     int t = 1;
     cin >> t;
-    while (t--) solve();
+    while (t--) 
+    {
+        ans=0;
+        int n;
+        cin>>n;
+        vector<vector<int>> v(n+1,vector<int>());
+        for(int i=0;i<n-1;i++){
+            int temp;
+            cin>>temp;
+            v[temp].push_back(i+2);
+        }
+        string s;
+        cin>>s;
+        dfs(1,v,s);
+        cout<<ans<<endl;
+
+    }
     return 0;
 }
