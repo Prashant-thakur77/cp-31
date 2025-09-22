e318c84f7dbb9b8ef4ad7145d046f2b294cf1a8e
Prashant-thakur77 <prashant101007@gmail.com>
2025-09-22 22:39:26 +0530

    done

--- 1300/14.cpp
+++ 1300/14.cpp
@@ -9,6 +9,45 @@ using namespace std;
 
 void solve() {
     // Your code here
+    ll n;
+    cin>>n;
+    vector<ll> no(n);
+    for(int i=0;i<n;i++){
+        cin>>no[i];
+    }
+    vector<int> v(31,0);
+    for(int i=0;i<n;i++){
+        ll num=no[i];
+        for(int j=0;j<30;j++){
+            if(((num&(1<<j)))){
+                v[j]++;
+
+            }
+
+        }
+    }
+    vector<int> ans;
+    for(int i=1;i<=n;i++){
+        bool ok=true;
+        for(int j=0;j<30;j++){
+            if(v[j]%i!=0){
+                ok=false;
+                break;
+            }
+        }
+        if(ok)ans.push_back(i);
+
+    }
+    /*for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;*/
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+    return;
+
 }
 
 int32_t main() {
