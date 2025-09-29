de52e1dd6c8f40d28d775ddc16f3fdce62b3325b
Prashant-thakur77 <prashant101007@gmail.com>
2025-09-29 19:49:24 +0530

    done

---
+++ 1300/24.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mod 1000000007
+
+void solve() {
+    // Your code here
+    ll n;
+    cin>>n;
+    vector<ll> v(n);
+    ll sum=0;
+    for(int i=0;i<n;i++){
+      cin>>v[i];
+      
+      
+    }
+     for(int i=0;i<n;i++){
+      
+      sum+=v[i];
+      if(sum<=0){
+        cout<<"no"<<endl;
+        return;
+      }
+    }
+    
+    sum=0;
+    for(int i=n-1;i>=0;i--){
+     
+      sum+=v[i];
+      if(sum<=0){
+        cout<<"no"<<endl;
+        return;
+      }
+    }
+    cout<<"yes"<<endl;
+    return;
+    
+    
+}
+
+int32_t main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
