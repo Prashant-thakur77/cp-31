87c517464be3655271c0444c495edfd72873bfbc
Prashant-thakur77 <prashant101007@gmail.com>
2025-10-03 23:32:48 +0530

    done

---
+++ codeforces/prac1.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mod 1000000007
+
+void solve() {
+    // Your code here
+    ll n,m,k,y,s=0;
+    cin>>n>>m>>k>>y;
+    
+    for(int i=0;i<n;i++)cin>>s;
+    for(int i=0;i<m;i++)cin>>s;
+    cout<<n+m<<endl;
+
+}
+
+int32_t main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
