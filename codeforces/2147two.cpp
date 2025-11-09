31482c1e85de25afc34072f3ce88a5ef3bd214ce
Prashant-thakur77 <prashant101007@gmail.com>
2025-11-09 15:11:14 +0530

    done

---
+++ codeforces/2147two.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mod 1000000007
+
+void solve() {
+    // Your code here
+    int n;
+    cin>>n;
+    vector<int> v(2*n,0);
+    int k=n;
+    for(int i=0;i<n;i++){
+      v[i]=k;
+      k--;
+
+    }
+    v[n]=n;
+    int h=1;
+    for(int i=n+1;i<2*n;i++){
+      v[i]=h;
+      h++;
+
+    }
+    for(int i=0;i<2*n;i++){
+     cout<<v[i]<<" ";
+
+    }
+    cout<<endl;
+
+
+}
+
+int32_t main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
