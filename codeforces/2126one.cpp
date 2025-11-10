825738ef92170799ff937bd0730d2080052b1c06
Prashant-thakur77 <prashant101007@gmail.com>
2025-11-10 17:26:31 +0530

    done

---
+++ codeforces/2126one.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mod 1000000007
+
+void solve() {
+    // Your code here
+    int n;
+    cin>>n;
+    string s=to_string(n);
+    int k=s.size();
+    int mini=2000;
+    for(int i=0;i<k;i++){
+      
+      mini=min(mini,s[i]-'0');
+    }
+    cout<<mini<<endl;
+    return;
+}
+
+int32_t main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
