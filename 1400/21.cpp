636b6b99c02885a129ca93608d0dd248be18108f
Prashant-thakur77 <prashant101007@gmail.com>
2025-11-12 22:13:43 +0530

    done

---
+++ 1400/21.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mod 1000000007
+
+void solve() {
+    // Your code here
+    ll n;
+    cin>>n;
+    ll ans=0;
+    for(int i=0;i<60;i++){
+      if(n&(1LL<<i)) ans+=(1LL<<(i+1))-1;
+    }
+    cout<<ans<<endl;
+
+}
+
+int32_t main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
