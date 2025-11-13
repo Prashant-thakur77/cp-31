f72414436b214e877b3e33317158b4150fed1cf0
Prashant-thakur77 <prashant101007@gmail.com>
2025-11-14 01:35:00 +0530

    done

---
+++ codeforces/2122.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mod 1000000007
+
+void solve() {
+    // Your code here
+    ll n,m;
+    cin>>n>>m;
+    if(n==1 || m==1){
+      cout<<"no"<<endl;
+    }
+    else if(n==2 && m==2)cout<<"no"<<endl;
+    else cout<<"yes"<<endl;
+
+}
+
+int32_t main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
