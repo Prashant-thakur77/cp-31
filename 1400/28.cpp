10c35b146a9cd34cd9cbc15b85b78c172ca1315a
Prashant-thakur77 <prashant101007@gmail.com>
2025-12-15 23:22:17 +0530

    done

---
+++ 1400/28.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mod 1000000007
+
+void solve() {
+    // Your code here
+}
+
+int32_t main() {
+    solve();
+    return 0;
+}
