0357851ad2ef9672c9626118c7b7b6af4b05c9de
Prashant-thakur77 <prashant101007@gmail.com>
2025-12-17 23:50:37 +0530

    done

--- 1400/29.cpp
+++ 1400/29.cpp
@@ -7,16 +7,37 @@ using namespace std;
 #define rall(x) (x).rbegin(), (x).rend()
 #define pb push_back
 #define mod 1000000007
+int ans[100000];
 
 void solve() {
     // Your code here
+    
     int n,m,k;
     cin>>n>>m>>k;
+    
     vector<int> v(n);
+    int y=0;
     for(int i=0;i<n;i++){
       cin>>v[i];
+      ans[i]=v[i]-y-1;
+      y=v[i];
+
     }
+    if(k>=n){
+      cout<<n<<endl;
+      return;
+    }
+    sort(ans+1,ans+n);
+    ll ans1=0;
+    for(int i=1;i<=n-k;i++){
+      ans1+=ans[i];
+
+    }
+    cout<<n+ans1<<endl;
+    return;
+
     
+
 }
 
 int32_t main() {
