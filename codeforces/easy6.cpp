62021130c28c2f02ae6619c7e932bc522709182a
Prashant-thakur77 <prashant101007@gmail.com>
2025-12-20 23:32:12 +0530

    done

---
+++ codeforces/easy6.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mod 1000000007
+
+void solve() {
+    // Your code here
+    ll n;cin>>n;
+    ll k=n/2;
+    if (n%2){cout<<k*(k+1)+1<<endl;return;}
+    cout<<k*k+1<<endl;
+}
+
+int32_t main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
