b593376dc4c300b3c2a30ec02b8abb8f46e3b772
Prashant-thakur77 <prashant101007@gmail.com>
2025-12-24 01:18:21 +0530

    done

---
+++ codeforces/easy10.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mod 1000000007
+
+void solve() {
+    // Your code here
+    int a; 
+    int b;
+    int  c;
+    int d ;
+    cin>>a>>b>>c>>d;
+    if(min(a, c) >= min(b, d)) cout<<"Gellyfish"<<endl;
+    else cout<<"Flower"<<endl;
+}
+
+int32_t main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
