e7ec2f752e2f12509016a263fb69e5f75cde829a
Prashant-thakur77 <prashant101007@gmail.com>
2025-12-28 21:25:53 +0530

    done

--- 1500/7.cpp
+++ 1500/7.cpp
@@ -3,53 +3,40 @@ using namespace std;
 
 #define ll long long
 #define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define pb push_back
 #define mod 1000000007
+
 ll rev(ll x){
-  ll r=0;
-  while(x>0){
-    r=r*10+x%10;
-    x=x/10;
-
-  }
-  return r;
+    ll r=0;
+    while(x){
+        r=r*10+x%10;
+        x/=10;
+    }
+    return r;
 }
+
 bool palin(ll x){
-  if(rev(x)==x){
-    return true;
-  }
-  else return  false;
-
-
-
+    return rev(x)==x;
 }
 
-void solve() {
-    // Your code here
-    
-}
+static int dp[50000][500];
 
-int32_t main() {
-  
-    int k=50000;
-    int l=500;
-    int  dp[k][l];
+int main(){
+    fast_io;
+
+    const int k=50000;
+    const int l=500;
+
     vector<int> pal;
     pal.push_back(0);
-    for(int i=0;i<2*k;i++){
-      if(palin(i)){
-        pal.push_back(i);
-      }
+    for(int i=0;i<k;i++){
+        if(palin(i)) pal.push_back(i);
     }
-    for(int j=1;j<l;j++)
-        dp[0][j]=1;
-    for(int i=1;i<k;i++)
-    {
+
+    for(int j=1;j<l;j++) dp[0][j]=1;
+
+    for(int i=1;i<k;i++){
         dp[i][0]=0;
-        for(int j=1;j<l;j++)
-        {
+        for(int j=1;j<l;j++){
             if(pal[j]<=i)
                 dp[i][j]=(dp[i][j-1]+dp[i-pal[j]][j])%mod;
             else
@@ -57,14 +44,10 @@ int32_t main() {
         }
     }
 
-    fast_io;
-    int t = 1;
-    cin >> t;
-    while (t--) {
-      int n;
-      cin>>n;
-      cout<<dp[n][l-1]<<endl;
-      
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        cout<<dp[n][l-1]<<'\n';
     }
     return 0;
 }
