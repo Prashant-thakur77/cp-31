3e8dd6ce46ea7116f7316b8f298c9880729e6e9f
Prashant-thakur77 <prashant101007@gmail.com>
2026-01-04 23:08:50 +0530

    Updated

---
+++ codeforces/easy17.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mod 1000000007
+
+void solve() {
+    // Your code here
+    int x, y, a;
+    cin >> x >> y >> a;
+    if (a % (x + y) < x) {
+        cout << "NO\n";
+    } else {
+        cout << "YES\n";
+    }
+}
+
+int32_t main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
