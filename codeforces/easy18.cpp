f33845ff8d9d48877ace19a4ca8bca8100740f56
Prashant-thakur77 <prashant101007@gmail.com>
2026-01-05 23:13:20 +0530

    Updated

---
+++ codeforces/easy18.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mod 1000000007
+
+void solve() {
+    // Your code here
+    int x,y;
+    cin>>x>>y;
+    if(x+1>=y&& (x+ 1-y)%9==0){
+      cout<<"yes"<<endl;
+    }
+    else cout<<"no"<<endl;
+}
+
+int32_t main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
