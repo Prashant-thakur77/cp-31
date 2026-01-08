bcc8f12bd23c08bf652a246ed701bc36f556beae
Prashant-thakur77 <prashant101007@gmail.com>
2026-01-08 21:47:32 +0530

    done

--- 1500/14.cpp
+++ 1500/14.cpp
@@ -10,7 +10,39 @@ using namespace std;
 
 void solve() {
     // Your code here
-    
+    ll n;
+    cin>>n;
+    vector<ll> v(n+1);
+    for(int i=1;i<=n;i++){
+        cin>>v[i];
+    }
+    vector<vector<ll>> adj(n+1);
+    for(int i=0;i<n-1;i++){
+        ll a,b;
+        cin>>a>>b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    ll val=0;
+    vector<ll> vec;
+    for(int i=1;i<=n;i++){
+        val+=v[i];
+        ll size=adj[i].size();
+        for(int j=0;j<size-1;j++){
+            vec.push_back(v[i]);
+
+
+        }
+
+
+    }
+    sort(rall(vec));
+    cout<<val<<" ";
+    for(auto i:vec){
+        val+=i;
+        cout<<val<<" ";
+    }
+    cout<<endl;
 }
 
 int32_t main() {
