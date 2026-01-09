a5e99e482bb5008057b7e548bbdf960e639bfefc
Prashant-thakur77 <prashant101007@gmail.com>
2026-01-09 23:15:31 +0530

    done

--- 1500/21.cpp
+++ 1500/21.cpp
@@ -20,12 +20,47 @@ void solve() {
     for(int i=0;i<m;i++){
       cin>>w[i];
     }
+    vector<ll> a(n+1,0);
+    vector<ll> b(m+1,0);
+    ll cnt=0;
+    for(int i=0;i<n;i++){
+      if(v[i]==1){
+        cnt++;
+        a[cnt]++;
+      }
+      else cnt=0;
+    }
+    cnt=0;
+    for(int i=0;i<m;i++){
+      if(w[i]==1){
+        cnt++;
+        b[cnt]++;
+      }
+      else cnt=0;
+    }
+    for(int i=n-1;i>=0;i--){
+      a[i]+=a[i+1];
+    }
+    for(int i=m-1;i>=0;i--){
+      b[i]+=b[i+1];
+    }
+    ll ans=0;
+    for(int i=1;i*i<=k;i++){
+      if(k%i==0){
+        ll j=k/i;
+        if(i<=n && j<=m){
+          ans+=a[i]*b[j];
+        }
+        if(i!=j && j<=n && i<=m){
+          ans+=a[j]*b[i];
+        }
+      }
+    }
+    cout<<ans<<endl;
 }
 
 int32_t main() {
     fast_io;
-    int t = 1;
-    cin >> t;
-    while (t--) solve();
+     solve();
     return 0;
 }
