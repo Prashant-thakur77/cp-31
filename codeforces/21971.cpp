965abe66452602ec39d7b724627525de94ef31f0
Prashant-thakur77 <prashant101007@gmail.com>
2026-02-13 09:42:38 +0530

    don

---
+++ codeforces/21971.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mod 1000000007
+
+void solve() {
+    // Your code here
+    ll n;
+    cin>>n;
+    if(n%9==0){
+      cout<<10<<endl;
+    }
+    else cout<<0<<endl;
+}
+
+int32_t main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
