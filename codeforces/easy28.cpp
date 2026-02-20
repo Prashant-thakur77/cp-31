16627688f0a65fce9c13165fbdc5cd1d0eb7d07a
Prashant-thakur77 <prashant101007@gmail.com>
2026-02-20 23:02:19 +0530

    done

---
+++ codeforces/easy28.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mod 1000000007
+
+void solve() {
+    // Your code here
+    ll n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+      cin>>v[i];
+    }
+    ll sum=0;
+    for(int i=0;i<n;i++){
+      sum+=v[i];
+    }
+    cout<<sum-n+1<<endl;
+}
+
+int32_t main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
