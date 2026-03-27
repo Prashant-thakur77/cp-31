07a07f60fd1b3770706a0df959369389bcb7a0fa
Prashant-thakur77 <prashant101007@gmail.com>
2026-03-27 20:05:19 +0530

    done

--- 1600/6.cpp
+++ 1600/6.cpp
@@ -7,37 +7,51 @@ using namespace std;
 #define rall(x) (x).rbegin(), (x).rend()
 #define pb push_back
 #define mod 1000000007
+int n;
+vector<bool> vis;
+vector<vector<int>> adj;
+void dfs(int i,int & cycles,int& len){
+  vis[i]=true;
+  len++;
+  if(adj[i].size()<2)cycles=false;
+  for(auto it:adj[i]){
+    if(vis[it])continue;
+    dfs(it,cycles,len);
+  }
+
+}
 
 void solve() {
     // Your code here
-    ll n;
+  
     cin>>n;
-    vector<bool> vis(n+1,false);
-    vector<int> v(n+1);
-    for(int i=1;i<=n;i++){
-      cin>>v[i];
+    vis.assign(n,false);
+    adj.assign(n,{});
+    for(int i=0;i<n;i++){
+      int no;
+      cin>>no;
+      no--;
+      adj[no].push_back(i);
+      adj[i].push_back(no);
 
     }
-    ll cycles=0;
-    ll cycles2=0;
-    for(int i=1;i<=n;i++){
-      if(!vis[i]){
-        int len=0;
-        int cur=i;
-        while(!vis[cur]){
-          vis[cur]=true;
-          cur=v[cur];
-          len++;
-        }
-        cycles++;
-        if(len==2){
-          cycles2++;
-        }
-      }
+    ll c=0;
+    ll bambo=0;
+    for(int i=0;i<n;i++){
+      if(vis[i])continue;
+      int cycles=1;
+      int len=0;
+      dfs(i,cycles,len);
+      if(cycles && len>2)c++;
+      else bambo++;
     }
-    ll maxi=cycles;
-    ll mini=cycles-cycles2+((cycles2>0)?1:0);
-    cout<<mini<<" "<<maxi <<endl;
+    if(bambo){
+      cout<<c+1<<" "<<c+bambo<<endl;
+    }
+    else{
+      cout<<c<<" "<<c<<endl;
+    }
+    
     return;
 
 }
