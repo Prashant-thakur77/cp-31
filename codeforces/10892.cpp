cd363025aa7e0c72bd78a8c035d2c8d72b8d73b2
Prashant-thakur77 <prashant101007@gmail.com>
2026-05-27 22:31:07 +0530

    done

---
+++ codeforces/10892.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mod 1000000007
+
+void solve() {
+    // Your code here
+    int n;
+    cin>>n;
+    vector<ll> v(n);
+    for(int i=0;i<n;i++){
+      cin>>v[i];
+    }
+    int cnt=0;
+    int j=1;
+    for(int i=0;i<n;i++){
+      if(v[i]<=j)cnt++;
+      j++;
+
+    }
+    cout<<cnt<<endl;
+}
+
+int32_t main() {
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
